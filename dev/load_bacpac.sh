be2362fe77b87fc99e4ef1f6dacbbb263e742051
Philipp36 <30549177+Philipp36@users.noreply.github.com>
2024-12-03 14:52:19 +0100

    MSSQL_HOST -> MSSQL_HOSTNAME

--- dev/load_bacpac.sh
+++ dev/load_bacpac.sh
@@ -13,13 +13,13 @@ if ! command -v sqlpackage &> /dev/null; then
 fi
 
 # check if connection to database is possible
-if ! sqlcmd -S $MSSQL_HOST,$MSSQL_PORT -U $MSSQL_USER -P $MSSQL_PASSWORD -C -Q "SELECT 1" &> /dev/null; then
-    echo "Connection to database at $MSSQL_HOST:$MSSQL_PORT failed for user $MSSQL_USER. Please check your connection settings."
+if ! sqlcmd -S $MSSQL_HOSTNAME,$MSSQL_PORT -U $MSSQL_USER -P $MSSQL_PASSWORD -C -Q "SELECT 1" &> /dev/null; then
+    echo "Connection to database at $MSSQL_HOSTNAME:$MSSQL_PORT failed for user $MSSQL_USER. Please check your connection settings."
     exit 1
 fi
 
 # if database exists, exit
-if sqlcmd -S $MSSQL_HOST,$MSSQL_PORT -U $MSSQL_USER -P $MSSQL_PASSWORD -C -Q "SELECT name FROM sys.databases WHERE name = '$MSSQL_DB'" | grep -q $MSSQL_DB; then
+if sqlcmd -S $MSSQL_HOSTNAME,$MSSQL_PORT -U $MSSQL_USER -P $MSSQL_PASSWORD -C -Q "SELECT name FROM sys.databases WHERE name = '$MSSQL_DB'" | grep -q $MSSQL_DB; then
     echo "Database $MSSQL_DB already exists. Please drop it before running $0 or start hacking."
     exit 0
 fi
